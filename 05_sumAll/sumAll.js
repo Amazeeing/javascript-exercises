4b74b94e54c03f75fe1218cbd80fbe2b20d4e263
Francesco <fplfrancesco@gmail.com>
2023-08-31 12:10:22 +0200

    Added solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(lowerBound, upperBound) {
+    if(lowerBound < 0 || upperBound < 0 || typeof lowerBound !== "number" || typeof upperBound !== "number") {
+        return "ERROR";
+    }
 
+    let result = 0;
+    if(lowerBound > upperBound) {
+        const temp = lowerBound;
+        lowerBound = upperBound;
+        upperBound = temp;
+    }    
+
+    for (let counter = lowerBound; counter <= upperBound; counter++) {
+       result += counter; 
+    }
+
+    return result;
 };
 
 // Do not edit below this line
